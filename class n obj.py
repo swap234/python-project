cbc034a0b29a141642b5e1262f711373db886376
Vijay <vijaykumar.kokkula@gmail.com>
2022-05-04 18:20:28 +0530

    OOPs

--- class n obj.py
+++ class n obj.py
@@ -1,10 +1,28 @@
 class person:
     pass
-def name(self):
-    print("Function inside the class")
-def roll_no(self):
-    print("Second function in the class")
-obj=Person
-print(obj.age)
+
+
+class Person:
+
+    def name(self):
+         print("function inside the class")
+    def roll_no(self):
+         print("Second function in the class")
+
+obj=Person()
 obj.name()
 obj.roll_no()
+
+class Parent:
+    pass
+class Parent:
+    def house(self):
+        print("It is asset")
+    def car(self):
+        print("It is liability")
+class child():
+    def bike(self):
+        print("It is good")
+obj=Parent()
+obj.car()
+obj.house()
